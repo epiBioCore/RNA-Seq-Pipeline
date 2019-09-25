6b2a66344d81078f773b45864bebdfd32e93ad32
Danielle Perley <danielle.perley@med.und.edu>
2019-09-25 11:07:46 -0500

    uncommented out most of the script

--- RNA-Seq.bash.sh
+++ RNA-Seq.bash.sh
@@ -172,252 +172,252 @@ bw_dir=($outDir/BigWigs)
 multiqc=($outDir/Multiqc)
 enrich=($outDir/Cluster_Analysis)
 
-################step1. FastQC
-#
-#
-#if [ ! -d $fastqc ]
-#then
-#	mkdir -p $fastqc
-#fi
-#
-#
-#
-##fastqc -t 10 -o $fastqc $fastq/*gz
-#
-#
-######### Step2. Remove adaptors
-#
-#
-#if [ ! -d $trimmed_fastq ]
-#then
-#	mkdir -p $trimmed_fastq
-#fi
-#
-#
-#while read -r sampleName coreNumber group r1 r2
-#do
-#	if [ $se != "true" ]
-#	then
-#
-#	read1=($fastq/${r1})
-#	read2=($fastq/${r2})
-#	i=$coreNumber
-#	echo trimming adaptors from $i
-#	 java -jar /usr/share/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads $threads -phred33 -trimlog $trimmed_fastq/Trimmomatic_${i}.log \
-#         $read1 $read2 $trimmed_fastq/${i}_1_at.fq.gz $trimmed_fastq/${i}_1_unpaired.fq.gz $trimmed_fastq/${i}_2_at.fq.gz $trimmed_fastq/${i}_2_unpaired.fq.gz \
-#         ILLUMINACLIP:/usr/share/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 2> $trimmed_fastq/${i}_trimStats.txt
-#	
-#	gzip $trimmed_fastq/Trimmomatic_${i}.log
-#	else
-#	
-#	read1=($fastq/${r1})
-#        i=$coreNumber
-#        echo trimming adaptors from $i
-#         java -jar /usr/share/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads $threads -phred33 -trimlog $trimmed_fastq/Trimmomatic_${i}.log \
-#         $read1  $trimmed_fastq/${i}_1_at.fq.gz \
-#         ILLUMINACLIP:/usr/share/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 2> $trimmed_fastq/${i}_trimStats.txt
-#	
-#	gzip $trimmed_fastq/Trimmomatic_${i}.log
-#	fi
-#
-#done < $samples
-#
-#
-#
-#
-####### FastQC again
-#
-#
-#
-# fastqc -t 10 -o $trimmed_fastq $trimmed_fastq/*at.fq.gz
-#
-#
-### STAR 1 pass
-#
-#if [ ! -d $star1index ]
-#then
-#	mkdir -p $star1index
-#fi
-#
-#
-#if [ ! -d $star1 ]
-#then
-#	mkdir -p $star1
-#
-#fi
-#
-#echo Generate index for STAR first pass
-#STAR --runThreadN $threads \
-#                 --runMode genomeGenerate \
-#                --genomeDir $star1index \
-#                --genomeFastaFiles $genome \
-#                --sjdbGTFfile $gtf \
-#                --sjdbOverhang $length
-#
-#
-#
-#
-#for i in $(ls $trimmed_fastq/*[12]_at.fq.gz | cut -f3 -d "/" | sed 's/_[12]_at.fq.gz//g' | sort | uniq)
-#do
-#       
-#	reads=$(ls $trimmed_fastq/${i}*_at.fq.gz)
-#	echo aligning $i for the first time
-#	STAR --runThreadN $threads \
-#                --genomeDir $star1index \
-#                --readFilesIn $reads \
-#                --readFilesCommand gunzip -c \
-#                --runMode alignReads \
-#                --outSAMattributes All \
-#                --alignSJoverhangMin 8 \
-#                --alignSJDBoverhangMin 1 \
-#                --outFilterMismatchNmax 999 \
-#                --outFilterMismatchNoverLmax 0.04 \
-#                --alignIntronMin 20 \
-#                --alignIntronMax 1000000 \
-#                --alignMatesGapMax 1000000 \
-#                --outSAMstrandField intronMotif \
-#                --outSAMtype BAM SortedByCoordinate \
-#                --outFileNamePrefix $star1/$i
-#
-#
-#done
-#
-#
-####### STAR 2 pass
-#
-#if [ ! -d $star2index ]
-#then
-#	mkdir -p $star2index
-#fi
-#
-#if [ ! -d $star2 ]
-#then
-#	mkdir -p $star2
-#fi
-#
-#SJ_gtfs=$(ls $star1/*SJ.out.tab)
-#
-#STAR --runThreadN $threads \
-#                --runMode genomeGenerate \
-#                --genomeDir $star2index \
-#                --genomeFastaFiles $genome \
-#                --sjdbFileChrStartEnd $SJ_gtfs \
-#                --sjdbGTFfile $gtf \
-#                --sjdbOverhang $length
-#
-#
-#
-#for i in $(ls $trimmed_fastq/*[12]_at.fq.gz | cut -f3 -d "/" | sed 's/_[12]_at.fq.gz//g' | sort | uniq)
-#do
-#
-#		reads=$(ls $trimmed_fastq/${i}*_at.fq.gz)
-#	        echo aligning $i for the second time
-#		
-#                STAR --runThreadN $threads \
-#                --genomeDir $star2index \
-#                --readFilesIn $reads \
-#                --readFilesCommand gunzip -c \
-#                --runMode alignReads \
-#                --outSAMattributes All \
-#                --alignSJoverhangMin 8 \
-#                --alignSJDBoverhangMin 1 \
-#                --outFilterMismatchNmax 999 \
-#                --outFilterMismatchNoverLmax 0.04 \
-#                --alignIntronMin 20 \
-#                --alignIntronMax 1000000 \
-#                --alignMatesGapMax 1000000 \
-#                --outSAMstrandField intronMotif \
-#                --outSAMtype BAM SortedByCoordinate \
-#                --outFileNamePrefix $star2/$i
-#
-#
-#		if [ $se != "true" ]
-#		then
-#
-#		samtools view -b -f 0x2 $star2/${i}Aligned.sortedByCoord.out.bam > $star2/${i}_properly_paired_sorted.bam
-#                samtools index $star2/${i}_properly_paired_sorted.bam
-#
-#		else
-#                
-#	        mv $star2/${i}Aligned.sortedByCoord.out.bam $star2/${i}_sorted.bam 
-#		samtools index $star2/${i}_sorted.bam 
-#	
-#
-#		fi
-#done
-#
-#
-#rm -r $star1 $star1index
-#
-#if [ "$bigwigs" = "true" ]
-#then
-#	if [ ! -d $bw_dir ]
-#	then
-#		mkdir -p "$bw_dir"
-#	fi
-#
-#
-#	for file in $star2/*_sorted.bam
-#	do
-#
-#		sample=$(echo $file | cut -f3 -d "/" | cut -f1 -d "_")
-#		
-#		bamCoverage -b $file --normalizeUsing CPM -of bigwig -o $bw_dir/${sample}.bw
-#	done
-#
-#fi
-#
-#
-#
-###get expression using cufflinks
-#
-#if [ ! -d $cufflinks ]
-#then
-#
-#	mkdir $cufflinks
-#fi
-#
-#lib=""
-#
-#
-#
-#if [ "$strand" = "rf" ]
-#then
-#lib="--library-type fr-firststrand"
-#
-#elif [ "$strand" = "fr" ]
-#then
-#
-#lib="--library-type fr-secondstrand"
-#
-#fi
-#
-#
-#
-#if [ "$strand" = "rf" ]
-#then
-#	lib="--library-type fr-firststrand"
-#elif [ "$strand" = "fr" ]
-#then
-#	lib="--library-type fr-secondstrand"
-#fi	
-#
-#for file in $star2/*sorted.bam
-#do
-#	echo "running cufflinks on $file"
-#
-#	sample=$(echo $file | cut -f3 -d "/" | cut -f1 -d "_")
-#	cufflinks -p $threads -o $cufflinks/$sample \
-#           -G $gtf \
-#           -b $genome \
-#           -u \
-#           $lib \
-#           $file 2> $cufflinks/${sample}_stderr.txt
-#done
-#
-#
-##combine
-#combineCufflinksFpkms.R in=$cufflinks
+###############step1. FastQC
+
+
+if [ ! -d $fastqc ]
+then
+	mkdir -p $fastqc
+fi
+
+
+
+#fastqc -t 10 -o $fastqc $fastq/*gz
+
+
+######## Step2. Remove adaptors
+
+
+if [ ! -d $trimmed_fastq ]
+then
+	mkdir -p $trimmed_fastq
+fi
+
+
+while read -r sampleName coreNumber group r1 r2
+do
+	if [ $se != "true" ]
+	then
+
+	read1=($fastq/${r1})
+	read2=($fastq/${r2})
+	i=$coreNumber
+	echo trimming adaptors from $i
+	 java -jar /usr/share/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads $threads -phred33 -trimlog $trimmed_fastq/Trimmomatic_${i}.log \
+         $read1 $read2 $trimmed_fastq/${i}_1_at.fq.gz $trimmed_fastq/${i}_1_unpaired.fq.gz $trimmed_fastq/${i}_2_at.fq.gz $trimmed_fastq/${i}_2_unpaired.fq.gz \
+         ILLUMINACLIP:/usr/share/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 2> $trimmed_fastq/${i}_trimStats.txt
+	
+	gzip $trimmed_fastq/Trimmomatic_${i}.log
+	else
+	
+	read1=($fastq/${r1})
+        i=$coreNumber
+        echo trimming adaptors from $i
+         java -jar /usr/share/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads $threads -phred33 -trimlog $trimmed_fastq/Trimmomatic_${i}.log \
+         $read1  $trimmed_fastq/${i}_1_at.fq.gz \
+         ILLUMINACLIP:/usr/share/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 2> $trimmed_fastq/${i}_trimStats.txt
+	
+	gzip $trimmed_fastq/Trimmomatic_${i}.log
+	fi
+
+done < $samples
+
+
+
+
+###### FastQC again
+
+
+
+ fastqc -t 10 -o $trimmed_fastq $trimmed_fastq/*at.fq.gz
+
+
+## STAR 1 pass
+
+if [ ! -d $star1index ]
+then
+	mkdir -p $star1index
+fi
+
+
+if [ ! -d $star1 ]
+then
+	mkdir -p $star1
+
+fi
+
+echo Generate index for STAR first pass
+STAR --runThreadN $threads \
+                 --runMode genomeGenerate \
+                --genomeDir $star1index \
+                --genomeFastaFiles $genome \
+                --sjdbGTFfile $gtf \
+                --sjdbOverhang $length
+
+
+
+
+for i in $(ls $trimmed_fastq/*[12]_at.fq.gz | cut -f3 -d "/" | sed 's/_[12]_at.fq.gz//g' | sort | uniq)
+do
+       
+	reads=$(ls $trimmed_fastq/${i}*_at.fq.gz)
+	echo aligning $i for the first time
+	STAR --runThreadN $threads \
+                --genomeDir $star1index \
+                --readFilesIn $reads \
+                --readFilesCommand gunzip -c \
+                --runMode alignReads \
+                --outSAMattributes All \
+                --alignSJoverhangMin 8 \
+                --alignSJDBoverhangMin 1 \
+                --outFilterMismatchNmax 999 \
+                --outFilterMismatchNoverLmax 0.04 \
+                --alignIntronMin 20 \
+                --alignIntronMax 1000000 \
+                --alignMatesGapMax 1000000 \
+                --outSAMstrandField intronMotif \
+                --outSAMtype BAM SortedByCoordinate \
+                --outFileNamePrefix $star1/$i
+
+
+done
+
+
+###### STAR 2 pass
+
+if [ ! -d $star2index ]
+then
+	mkdir -p $star2index
+fi
+
+if [ ! -d $star2 ]
+then
+	mkdir -p $star2
+fi
+
+SJ_gtfs=$(ls $star1/*SJ.out.tab)
+
+STAR --runThreadN $threads \
+                --runMode genomeGenerate \
+                --genomeDir $star2index \
+                --genomeFastaFiles $genome \
+                --sjdbFileChrStartEnd $SJ_gtfs \
+                --sjdbGTFfile $gtf \
+                --sjdbOverhang $length
+
+
+
+for i in $(ls $trimmed_fastq/*[12]_at.fq.gz | cut -f3 -d "/" | sed 's/_[12]_at.fq.gz//g' | sort | uniq)
+do
+
+		reads=$(ls $trimmed_fastq/${i}*_at.fq.gz)
+	        echo aligning $i for the second time
+		
+                STAR --runThreadN $threads \
+                --genomeDir $star2index \
+                --readFilesIn $reads \
+                --readFilesCommand gunzip -c \
+                --runMode alignReads \
+                --outSAMattributes All \
+                --alignSJoverhangMin 8 \
+                --alignSJDBoverhangMin 1 \
+                --outFilterMismatchNmax 999 \
+                --outFilterMismatchNoverLmax 0.04 \
+                --alignIntronMin 20 \
+                --alignIntronMax 1000000 \
+                --alignMatesGapMax 1000000 \
+                --outSAMstrandField intronMotif \
+                --outSAMtype BAM SortedByCoordinate \
+                --outFileNamePrefix $star2/$i
+
+
+		if [ $se != "true" ]
+		then
+
+		samtools view -b -f 0x2 $star2/${i}Aligned.sortedByCoord.out.bam > $star2/${i}_properly_paired_sorted.bam
+                samtools index $star2/${i}_properly_paired_sorted.bam
+
+		else
+                
+	        mv $star2/${i}Aligned.sortedByCoord.out.bam $star2/${i}_sorted.bam 
+		samtools index $star2/${i}_sorted.bam 
+	
+
+		fi
+done
+
+
+rm -r $star1 $star1index
+
+if [ "$bigwigs" = "true" ]
+then
+	if [ ! -d $bw_dir ]
+	then
+		mkdir -p "$bw_dir"
+	fi
+
+
+	for file in $star2/*_sorted.bam
+	do
+
+		sample=$(echo $file | cut -f3 -d "/" | cut -f1 -d "_")
+		
+		bamCoverage -b $file --normalizeUsing CPM -of bigwig -o $bw_dir/${sample}.bw
+	done
+
+fi
+
+
+
+##get expression using cufflinks
+
+if [ ! -d $cufflinks ]
+then
+
+	mkdir $cufflinks
+fi
+
+lib=""
+
+
+
+if [ "$strand" = "rf" ]
+then
+lib="--library-type fr-firststrand"
+
+elif [ "$strand" = "fr" ]
+then
+
+lib="--library-type fr-secondstrand"
+
+fi
+
+
+
+if [ "$strand" = "rf" ]
+then
+	lib="--library-type fr-firststrand"
+elif [ "$strand" = "fr" ]
+then
+	lib="--library-type fr-secondstrand"
+fi	
+
+for file in $star2/*sorted.bam
+do
+	echo "running cufflinks on $file"
+
+	sample=$(echo $file | cut -f3 -d "/" | cut -f1 -d "_")
+	cufflinks -p $threads -o $cufflinks/$sample \
+           -G $gtf \
+           -b $genome \
+           -u \
+           $lib \
+           $file 2> $cufflinks/${sample}_stderr.txt
+done
+
+
+#combine
+combineCufflinksFpkms.R in=$cufflinks
 
 
 
